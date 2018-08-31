9991b29d74079294d2b40a360b3eabb39ed7175a
moremagic <itoumagic@gmail.com>
2018-08-31 17:13:36 +0900

    code追加

---
+++ chapter-8/code.lisp
@@ -0,0 +1,7 @@
+(defun primep (number)
+  (when (> number 1)
+    (loop for fac from 2 to (isqrt number) never (zerop (mod number fac)))))
+
+(defun next-prime (number)
+  (loop for n from number (when (primep n) return n)))
+
